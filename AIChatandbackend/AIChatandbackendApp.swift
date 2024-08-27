bf277c91bff8f3aba315e3c48f0fb3544be49cd2
Jason Wan <njasow@g.ucla.edu>
2024-08-26 20:15:17 -0700

    Initial Commit

---
+++ AIChatandbackend/AIChatandbackendApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatandbackendApp.swift
+//  AIChatandbackend
+//
+//  Created by jason wan on 2024-08-26.
+//
+
+import SwiftUI
+
+@main
+struct AIChatandbackendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
